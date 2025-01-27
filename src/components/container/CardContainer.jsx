9f3f6bbb27580c3dd979a7c2eced0d59ac357a5b
caroDesign <juego.fuego@gmail.com>
2025-01-26 23:05:34 -0500

    Animations added

--- src/components/container/CardContainer.jsx
+++ src/components/container/CardContainer.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import { motion } from 'motion/react'
 import Icon from '../pure/Icon'
 import Button from '../pure/Button'
 
@@ -6,26 +7,35 @@ function CardContainer({ id, title, description, icon, btnText }) {
 
   let bgColor
   let txtColor
+  let delay
   switch (id) {
     case 1:
       bgColor = 'bg-bright-orange'
       txtColor = 'text-bright-orange'
+      delay = 0.5
       break
     case 2:
       bgColor = 'bg-dark-cyan'
       txtColor = 'text-dark-cyan'
+      delay = 1
       break
     case 3:
       bgColor = 'bg-very-dark-cyan'
       txtColor = 'text-very-dark-cyan'
+      delay = 1.5
       break
     default:
       break
   }
 
+  const viewport = window.screen.width
+
   return (
-    <article
+    <motion.article
       className={(`${bgColor} first:rounded-t-lg last:rounded-b-lg md:first:rounded-tr-none ${id < 3 && 'md:w-1/2'} ${id === 2 && 'md:rounded-tr-lg lg:rounded-tr-none'} lg:w-full lg:first:rounded-bl-lg lg:last:rounded-bl-none lg:last:rounded-tr-lg`).replaceAll('false', '').split(' ').filter(item => item !== '').join(' ')}
+      initial={viewport < 1024 ? { opacity: 0 } : { opacity: 0, translateY: 100 }}
+      animate={viewport < 1024 ? { opacity: 1 } : { opacity: 1, translateY: 0 }}
+      transition={{ duration: 0.5, delay: delay }}
     >
       <Icon icon={icon} alt={title} />
       <h1
@@ -39,7 +49,7 @@ function CardContainer({ id, title, description, icon, btnText }) {
         {description}
       </p>
       <Button text={btnText} color={txtColor} />
-    </article>
+    </motion.article>
   )
 }
 
